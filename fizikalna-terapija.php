9f3af1197ac1ccb3e1f833651e532e3868cbb101
UrosZikic <uroszikic1512988@gmail.com>
2024-01-06 15:54:52 +0100

    optimizacija koda

--- fizikalna-terapija.php
+++ fizikalna-terapija.php
@@ -1,44 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="cards.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Onest:wght@300;400;500&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZuuL25t9l8Ftxnhwq1qj0i8/cq4knpDhvNyZxOeOW/6v5BtQJ0eP7JFObjimZx" crossorigin="anonymous" />
-    <link rel="icon" type="image/png" href="icons/spinelogo.jpg" id="customFavicon">
-</head>
-<body>
-    
-    <?php include 'navbar.php'; ?>
-    <div class="container">
-        <h1 class="text-center my-5">Usluge</h1>
+<?php
+include "head.php";
+include 'navbar.php'; ?>
+<div class="container">
+    <h1 class="text-center my-5">Usluge</h1>
 
-        <div>
-            <h2>Fizikalna terapija</h2>
+    <div>
+        <h2>Fizikalna terapija</h2>
 
-            <h3 class="py-4">TRANSKUTANA ELEKTRIČNA NERVNA STIMULACIJA (TENS)</h3>
+        <h3 class="py-4">TRANSKUTANA ELEKTRIČNA NERVNA STIMULACIJA (TENS)</h3>
 
-            <h4 class="py-3">STA JE TENS I KOJE SU NJEGOVE VRSTE?</h4>
-            <p class="py-2 fs-5">To je jedna od metoda elektricne stimulacije ciji je primarni cilj da obezbedi odredjeni stepen simptomatskog ublazavanja bola uz pomoc uzbudljivih senzornih nerava. <br> TENS jedinice rade tako sto isporucuju male elektricne impulse kroz elektrode koje imaju lepljive jastucice za pricvrscivanje na kozu osobe. <br> Ovi elektricni impulsi preplavljuju nervni sistem, smanjujuci njegovu sposobnost da prenosi signale bola do kicmene mozdine i mozga.</p>
+        <h4 class="py-3">STA JE TENS I KOJE SU NJEGOVE VRSTE?</h4>
+        <p class="py-2 fs-5">To je jedna od metoda elektricne stimulacije ciji je primarni cilj da obezbedi odredjeni
+            stepen simptomatskog ublazavanja bola uz pomoc uzbudljivih senzornih nerava. <br> TENS jedinice rade tako
+            sto isporucuju male elektricne impulse kroz elektrode koje imaju lepljive jastucice za pricvrscivanje na
+            kozu osobe. <br> Ovi elektricni impulsi preplavljuju nervni sistem, smanjujuci njegovu sposobnost da prenosi
+            signale bola do kicmene mozdine i mozga.</p>
 
-            <h4 class="py-3">Uopsteno je kategorisan u 3 tipa:</h4>
-            <p><b>Konvencionalni TENS (HIGH):</b> <br> Ukljucuje primenu struje niskog intenziteta/visoke frekvencije da bi se proizveo jak, ali ugodan osecaj, njegovo trajanje je 30 min. <br>
-                <b>TENS nalik akupunkturi (LOV):</b><br> Ukljucuje primenu struje visokog intenziteta / niske frekvencije preko misica ili akupunkturnih tacaka da bi se proizveo jak, ali udoban osecaj. <br> Njegovo trajanje je manje od 20 minuta. <br>
-                <b>Kratki intenzivni TENS:</b><br> Ukljucuje primenu struje visokog intenziteta / visoke frekvencije. Traje ne vise od 5 minuta.</p>
-        </div>
+        <h4 class="py-3">Uopsteno je kategorisan u 3 tipa:</h4>
+        <p><b>Konvencionalni TENS (HIGH):</b> <br> Ukljucuje primenu struje niskog intenziteta/visoke frekvencije da bi
+            se proizveo jak, ali ugodan osecaj, njegovo trajanje je 30 min. <br>
+            <b>TENS nalik akupunkturi (LOV):</b><br> Ukljucuje primenu struje visokog intenziteta / niske frekvencije
+            preko misica ili akupunkturnih tacaka da bi se proizveo jak, ali udoban osecaj. <br> Njegovo trajanje je
+            manje od 20 minuta. <br>
+            <b>Kratki intenzivni TENS:</b><br> Ukljucuje primenu struje visokog intenziteta / visoke frekvencije. Traje
+            ne vise od 5 minuta.
+        </p>
     </div>
+</div>
 
 
-
-    <script src="https://kit.fontawesome.com/ae81086103.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+include "footer.php";
+?>
---
+++ head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>My Spine</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="cards.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Onest:wght@300;400;500&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZuuL25t9l8Ftxnhwq1qj0i8/cq4knpDhvNyZxOeOW/6v5BtQJ0eP7JFObjimZx" crossorigin="anonymous" />
+  <link rel="icon" type="image/png" href="icons/spinelogo.jpg" id="customFavicon" />
+</head>
+
+<body>
