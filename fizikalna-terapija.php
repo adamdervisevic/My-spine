8a8381da6e4ad4a6048227f76ef8d903fc306ec3
adamdervisevic <adamdervisevic@yahoo.com>
2024-01-10 23:24:52 +0100

    jos teksa :(

--- fizikalna-terapija.php
+++ fizikalna-terapija.php
@@ -3,12 +3,9 @@ include "head.php";
 include 'navbar.php'; ?>
 <div class="container">
     <h1 class="text-center my-5">Usluge</h1>
-
-    <div>
-        <h2>Fizikalna terapija</h2>
-
-        <h3 class="py-4">TRANSKUTANA ELEKTRIČNA NERVNA STIMULACIJA (TENS)</h3>
-
+    <h2 class="text-center">Fizikalna terapija</h2>
+    <div class="TENS">
+        <h3 class="py-4 text-center">TRANSKUTANA ELEKTRIČNA NERVNA STIMULACIJA (TENS)</h3>
         <h4 class="py-3">STA JE TENS I KOJE SU NJEGOVE VRSTE?</h4>
         <p class="py-2 fs-5">To je jedna od metoda elektricne stimulacije ciji je primarni cilj da obezbedi odredjeni
             stepen simptomatskog ublazavanja bola uz pomoc uzbudljivih senzornih nerava. <br> TENS jedinice rade tako
@@ -17,7 +14,7 @@ include 'navbar.php'; ?>
             signale bola do kicmene mozdine i mozga.</p>
 
 
-            <h4 class="py-3">Uopsteno je kategorisan u 3 tipa:</h4>
+            <h4 class="py-4">Uopsteno je kategorisan u 3 tipa:</h4>
             <p class="py-2 fs-5"><b>Konvencionalni TENS (HIGH):</b> <br> Ukljucuje primenu struje niskog intenziteta/visoke frekvencije da bi se proizveo jak, ali ugodan osecaj, njegovo trajanje je 30 min. <br>
                 <b>TENS nalik akupunkturi (LOV):</b><br> Ukljucuje primenu struje visokog intenziteta / niske frekvencije preko misica ili akupunkturnih tacaka da bi se proizveo jak, ali udoban osecaj. <br> Njegovo trajanje je manje od 20 minuta. <br>
                 <b>Kratki intenzivni TENS:</b><br> Ukljucuje primenu struje visokog intenziteta / visoke frekvencije. <br> Traje ne vise od 5 minuta.</p>
@@ -31,7 +28,7 @@ include 'navbar.php'; ?>
             <h3 class="py-4">KOJE SIMPTOME I STANJA LECI TENS?</h3>
             <p class="py-2 fs-5">TENS jedinica ima kontrole koje omogucavaju ljudima da daju odgovarajuci nivo ublazavanja bola. <br> Moze se postici promenom intenziteta, frekvencije i trajanja impulsa. <br> Moze pomoci u lecenju sledecih simptoma:</p>
 
-            <ul?>
+            <ul>
                 <li>Sindromi hronicnog bola</li>
                 <li>Period Pain</li>
                 <li>Labor Pain</li>
@@ -81,9 +78,108 @@ include 'navbar.php'; ?>
             Moze se koristiti vise pristupa. <br> Elektrode se mogu postaviti preko
             Akupunkturne tacke, motoricke tacke ili triger tacke. <br>
             Podrucje najveceg intenziteta bola odgovarajuci dermatom ili segment kicme odgovarajuci periferni nerv koji god polozaj da se izabere za postavljanje elektroda, najbolje je da koza ispod njih ima netaknut senzorni mehanizam jer aferentna senzorna stimulacija velikog precnika koju proizvodi TNS struja koja deluje na kozu proizvodi efekat na bol. <br> Kada su elektrode postavljene, TENS se moze primeniti podesavanjem parametara.</p>
-        </div>
-        
     </div>
+    <div class="IFT">
+        <h3 class="text-center py-4">INTERFERENTNA TERAPIJA</h3>
+        <h4 class="py-4">STA JE INTERFERENTNA TERAPIJA (IFT)?</h4>
+        <p class="py-2 fs-5">IFT, sto znaci interferentna terapija, jedna je od vrsta elektroterapije koja se koristi za upravljanje bolom. <br> Princip interferentne terapije je da izazove da dve srednje frekvencijske struje malo razlicitih frekvencija interferiraju jedna sa drugom. <br> Na primer, ako kolo A nosi struju sa frekvencijom od 4000 Hz, a kolo B nosi struju sa frekvencijom od 3980 Hz, tada ce proizvedena niska frekvencija biti 20 Hz i ova frekvencija je veoma korisna u modulaciji bola. <br> Nova niskofrekventna struja poznata kao frekvencija otkucaja jednaka je razlici u frekvencijama izmedju dve srednje frekvencijske struje proizvedene u tkivima na mestu gde se dve struje ukrstaju. <br> U osnovi se koristi za lecenje hronicnih, posttraumatskih i post-hirurskih bolova. <br> Osnovni princip podrazumeva koriscenje efekata niskih frekvencija (manje od 250pps) bez bolnih ili neprijatnih nuspojava. <br> Glavna prednost IFT-a je da proizvodi efekte u tkivu, tacno tamo gde je potrebno, bez nepotrebne i neprijatne stimulacije koze. <br> Ova tehnika se siroko koristi za izazivanje kontrakcije misica, podsticanje zarastanja i smanjenje edema. </p>
+
+        <h4 class="py-4">KOJE SU VRSTE INTERFERENTNIH TERAPIJA?</h4>
+        <p class="py-2 fs-5"><b>Vektorski efekat</b> <br>
+        Interferentno polje se rotira pod uglom od 450 u svakom pravcu, tako da polje pokriva siru oblast. <br> Ovo je korisno kod difuzne patologije ili ako se mesto lezije ne moze tacno lokalizovati.</p>
+
+        <p class="py-2 fs-5"><b>Promene frekvencije</b> <br>
+        Neka oprema dozvoljava varijaciju u brzini promene frekvencije. <br> Ritmicki rezim moze biti kontinuirano ljuljanje od 0 do 100 Hz za 5-10 sekundi i nazad u slicnom vremenu ili moze trajati 1-6 sekundi na jednoj frekvenciji praceno 1-6 s na drugoj frekvenciji sa promenljivim vremenom za ljuljanje izmedju dva.</p>
+
+        <p class="py-2 fs-5"><b>Konstantna frekvencija</b> <br>
+        Neki tretmani se mogu izvesti sa smetnjom fiksiranom na odredjenoj frekvenciji.<br> Ritmicka frekvencija je korisna ako treba tretirati nekoliko vrsta tkiva odjednom. <br>Varijacija u frekvenciji takodje prevazilazi problem akomodacije tkiva gde odgovor odredjenog tkiva opada sa vremenom.</p>
+
+        <p class="py-2 fs-5"><b>Sveep frekvencije</b> <br>
+        Princip koriscenja sveep-a je da je masina podesena da automatski menja frekvenciju stimulacije, koristi ili unapred podesene ili korisnicki podesene opsege sveep-a.<br> Opseg zahvata treba da odgovara zeljenim fizioloskim efektima. <br> Obrazac sveep-a cini znacajnu razliku u stimulaciji koju prima pacijent. <br>Vecina masina nudi nekoliko sema za ciscenje: <br>
+        Trouglasti uzorak. <br>
+        Rectangular Sveep Pattern. <br>
+        Trapezoidni Sveep Pattern.<br>
+        U 'trouglastom' obrascu sveep-a, masina se postepeno menja od osnovne do gornje frekvencije, tokom 6 sekundi.<br> Iako neke masine nude opcije od 1 ili 3 sekunde.<br> Sve frekvencije izmedju osnovne i gornje frekvencije se isporucuju u jednakoj proporciji.<br>
+        Pravougaoni sveep je drugaciji obrazac stimulacije u kojem su podesene osnovna i gornja frekvencija, ali masina se tada „prebacuje“ izmedju ove dve specificne frekvencije umesto da se postepeno menja sa jedne na drugu nivoa frekvencije, a drugi ce se prebacivati sa jedne frekvencije na drugu.<br> 'Trapezoidni' potez je zapravo kombinacija ova dva.</p>
+
+        <h4 class="py-4">KAKO FUNKCIONISE INTERFERENTNA TERAPIJA (IFT)?</h4>
+        <p class="py-2 fs-5">Terapija interferentnom strujom deluje tako sto salje male kolicine elektricne stimulacije ostecenim tkivima u telu. <br> Terapija ima za cilj da podstakne prirodni proces tela da reaguje na bol, povecavajuci cirkulaciju i na taj nacin proizvodi hormone koji podsticu zarastanje.<br> IFT isporucuje isprekidane impulse da stimulise povrsinske nerve i blokira signal bola, isporucujuci kontinuiranu duboku stimulaciju u zahvaceno tkivo.<br> IFT ublazava bol, povecava cirkulaciju, smanjuje edem i stimulise misice.<br> Frekvencija od 100Hz moze stimulisati A-beta vlakna velikog precnika, koja uticu na kapiju bola, i inhibiraju prenos nociceptivnog saobracaja malog precnika (C i A-delta vlakna), sto efikasno zatvara hod bolnim impulsima.<br> Interferencijalna struja povecava cirkulaciju krvi cime se smanjuje otok, ispiranjem hemikalija koje stimulisu nociceptivne nervne zavrsetke.<br>
+        Frekvencija izmedju 1 i 100 Hz se koristi za kontrakciju normalno inerviranih misica.<br> Razlicite vrste frekvencija izazivaju razlicite kontrakcije, niska frekvencija, izaziva trzanje, frekvencija izmedju 5 i 20 Hz izaziva delimicnu kontrakciju tetanije, a frekvencija od 30 do 100 Hz izaziva tetanicnu kontrakciju. <br> IFT takodje pomaze u smanjenju edema.<br> Frekvencija od 100Hz promovise vazodilataciju, cime se smanjuje edem.<br> A frekvencija od 10 Hz aktivira misicno-skeletnu pumpu, koja se koristi za promociju venskog i limfnog povratka izazivajuci smanjenje edema.<br> Neuroni koji cine autonomni nervni sistem su mali i slabo mijelinizovani poput Aδ i C vlakana perifernog nervnog sistema i da bi stimulisali autonomni nervni sistem, nize frekvencije se koriste za povecanje protoka krvi.</p>
+        
+        <h4 class="py-4">KOJI SU PARAMETRI TRETMANA INTERFERENTNE TERAPIJE (IFT)?</h4>
+        <p class="py-2 fs-5">Efekat IFT zavisi od: <br>
+        Frekvencija <br>
+        Intenzitet struje <br>
+        Postavljanje elektroda <br>
+        Frekvencija <br>
+        100-150Hz konstantna ili 90-100Hz ritmicna: <br> Ove frekvencije su korisne za ublazavanje bolova, IFT deluje na velika mijelinizovana vlakna u dorzalnom rogu, blokirajuci mala vlakna bola. <br> Vise frekvencije imaju pokretacki efekat na endogene opijate u srednjem mozgu koji inhibiraju bol.</p>
+
+        <p class="py-2 fs-5">0-10Hz ritmicno ili 10-50Hz ritmicno: <br> Ove frekvencije su korisne za stimulaciju misica. <br> Oni proizvode stimulaciju dubokog, normalno inerviranog misicnog tkiva sa malo senzorne stimulacije. <br> Kako se frekvencija povecava, kontrakcija se menja od trzanja do tetanicke kontrakcije.</p>
+
+        <p class="py-2 fs-5">0-100Hz ritmicki: Opseg frekvencija proizvodi vazodilataciju. <br> Ovo je stimulacija i opustanje zidova krvnih sudova, dajuci sinusni efekat.</p>
+
+        <p class="py-2 fs-5">Intenzitet struje <br>
+        Struja se povecava sve dok pacijent ne oseti peckanje, dalje kako dolazi do akomodacije intenzitet se polako povecava do tacke stimulacije misica. <br> Trajanje tretmana varira izmedju 10-20 minuta.</p>
+
+        <p class="py-2 fs-5">Postavljanje elektroda <br>
+        Pacijent se postavlja u udoban polozaj i koza se priprema za tretman. <br> Koza se cisti, u slucaju bilo kakve lezije koze, podrucje se izoluje vazelinom. <br> Vakum elektrode ili elektrode podloge se tacno postavljaju na mesto tretmana i dva para elektroda se postavljaju tako da tacka ukrstanja dve struje bude iznad ili unutar lezije.</p>
+
+        <h4 class="py-4">KOJA SU STANJA ILI SIMPTOMI KOJI SE LECE INTERFERENTNOM TERAPIJOM (IFT)?</h4>
+        <p class="py-2 fs-5">IFT radi na poseban nacin jer koristi interferencijalnu, a ne normalnu stimulaciju. <br> Glavne klinicke aplikacije za koje se cini da se IFT koristi su:</p>
+
+        <ul>
+            <li>Ublazavanje bolova kod stanja kao sto su kauzalgija, herpes zoster i neuralgija.</li>
+            <li>Grlica materice spondiloza.</li>
+            <li>Osteoartritis kolena.</li>
+            <li>Ankilozni spondilitis</li>
+            <li>Reumatoidni artritis.</li>
+            <li>Smrznuto rame.</li>
+            <li>Disk hernija.</li>
+            <li>Stenoza kicmenog kanala.</li>
+            <li>Stimulacija misica - sprecava gubitak misica, ponovno obrazovanje, odrzava opseg inkontinencije pri stresu pokreta</li>
+            <li>Smanjenje edema (stanje koje ukljucuje prekomerno sakupljanje vodene tecnosti u supljinama ili tkivima)</li>
+            <li>Povrede misica</li>
+            <li>Povrede ligamenta</li>
+        </ul>
+
+        <h4 class="py-4">KOJI SU FIZIOLOSKI EFEKTI INTERFERENTNE TERAPIJE (IFT)?</h4>
+        <p class="py-2 fs-5">Interferentna terapija se siroko koristi, ali fizioterapeut treba da zna kako da je koristi u zavisnosti od stanja. <br> Interferentna terapija koristi znacajne fizioloske efekte niske frekvencije (manje od 250pps) elektricne stimulacije nerava. <br>
+        Efekti interferentne terapije:</p>
+
+        <ul>
+            <li>Smanjuje bol i upalu.</li>
+            <li>Povecava cirkulaciju krvi.</li>
+            <li>Izaziva vazodilataciju.</li>
+            <li>Uklanja otpadne materije iz pogodjenog podrucja.</li>
+            <li>Povecava brzinu metabolizma.</li>
+            <li>Smanjuje krvni pritisak.</li>
+            <li>Leci hronicne lezije ligamenta.</li>
+            <li>Leci edem i hematom.</li>
+            <li>Poboljsava ogranicene pokrete zglobova.</li>
+            <li>Povecava stimulaciju misica.</li>
+            <li>Vraca izgubljeno kretanje misica.</li>
+        </ul>
+
+        <h4 class="py-4">KOJE SU KONTRAINDIKACIJE ZA INTERFERENTNU TERAPIJU (IFT)?</h4>
+        <p class="py-2 fs-5">IFT je uobicajeni modalitet za ublazavanje bolova bez nezeljenih efekata koje imaju oralni lekovi protiv bolova i antiinflamatorni lekovi. <br> Kao i svaki drugi terapeutski modalitet, on takodje ima neka ogranicenja kao sto su: <br> </p>
+
+        <ul>
+            <li>Infektivno stanje</li>
+            <li>Malignost.</li>
+            <li>Pejsmejkeri.</li>
+            <li>Gubitak osecaja.</li>
+            <li>Opasnost od krvarenja</li>
+            <li>Velike otvorene rane.</li>
+            <li>Dermatoloska stanja.</li>
+            <li>Pregnant Uterus.</li>
+            <li>Arterijska bolest.</li>
+            <li>Febrilno stanje.</li>
+            <li>Tromboza dubokih Vena.</li>
+            <li>Tokom menstruacije.</li>
+            <li>Nepouzdan pacijent.</li>
+        </ul>
+
+</div>
 </div>
 
 
