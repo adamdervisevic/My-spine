d7b0836a90547a3346ca620f59eabda6054e0b75
UrosZikic <uroszikic1512988@gmail.com>
2024-01-13 19:32:05 +0100

    logo change

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container">
     <a class="navbar-brand" href="#">
-      <img src="icons/logo.webp" alt="logo" class="d-inline-block align-text-top" />
+      <img src="icons/logo.png" alt="logo" class="d-inline-block align-text-top" />
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
