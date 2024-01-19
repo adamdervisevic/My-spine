1d5c39bb11656fead23e7c00cb57bf378aba2b67
adamdervisevic <adamdervisevic@yahoo.com>
2024-01-19 16:35:11 +0100

    par slova

--- akupunktura.php
+++ akupunktura.php
@@ -5,12 +5,12 @@ include 'navbar.php'; ?>
 <div class="container">
     <h1 class="text-center my-5">Usluge</h1>
     <h2 class="text-center">Akupunktura</h2>
-    <h4 class="py-3">Sta je akupunktura ili terapija suvim iglama?</h4>
+    <h4 class="py-3">Šta je akupunktura ili terapija suvim iglama?</h4>
 
     <p class="py-2 fs-5">Akupunktura je proces u kojem se tanke igle ubacuju u okidačke tačke ili zategnute mišiće na telu. <br> Glavni cilj je poboljšanje zdravlja tkiva i obnavljanje funkcije mišića. <br> Naši mišići ponekad razvijaju čvorove, takozvane okidačke tačke koji mogu biti vrlo osetljivi i bolni na dodir. <br>
-    Fizioterapeut ubacuje tanke čvrste igle kroz kožu u okidačke tačke. <br> Ove igle se koriste za stimulaciju tkiva, a ne za ubrizgavanje lekova. <br> Pacijenti mogu osetiti različite senzacije poput bolova u mišićima, tržanja mišića i sl prilikom ubacivanja igle, što smatramo pozitivnim znakom. <br> Igle mogu biti postavljene duboko ili površinski, na kraće ili duže periode, zavisno od tipa bola koji se leči.</p>
+    Fizioterapeut ubacuje tanke čvrste igle kroz kožu u okidačke tačke. <br> Ove igle se koriste za stimulaciju tkiva, a ne za ubrizgavanje lekova. <br> Pacijenti mogu osetiti različite senzacije poput bolova u mišićima, trzanja mišića i sl. prilikom ubacivanja igle, što smatramo pozitivnim znakom. <br> Igle mogu biti postavljene duboko ili površinski, na kraće ili duže periode, zavisno od tipa bola koji se leči.</p>
 
-    <p class="py-2 fs-5">Stanja koja se leče terapijom suvim iglama</p>
+    <p class="py-2 fs-5"><b>Stanja koja se leče terapijom suvim iglama</b></p>
 
     <p class="py-2 fs-5">Ova terapija pomaže u lečenju:</p>
 
@@ -19,13 +19,13 @@ include 'navbar.php'; ?>
         <li>bola u vratu</li>
         <li>bola u laktu</li>
         <li>disfunkcije kičme i zglobova</li>
-        <li>migrane</li>
+        <li>migrena</li>
         <li>išijasa</li>
         <li>problemi sa vilicom i ustima</li>
-        <li>oremećaja ponavljajućih pokreta</li>
+        <li>poremećaja ponavljajućih pokreta</li>
         <li>bolova u karlici</li>
         <li>noćnih grčeva</li>
-        <li>post-herpetićne neuralgije</li>
+        <li>post-herpetične neuralgije</li>
     </ul>
 </div>
 
