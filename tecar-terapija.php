61aafdd684bc71e66c5193d66edecc3f5f3912ca
adamdervisevic <adamdervisevic@yahoo.com>
2024-11-19 12:47:42 +0700

    update

---
+++ tecar-terapija.php
@@ -0,0 +1,58 @@
+<?php
+include "head.php";
+include "navbar.php"; ?>
+
+
+<div class="container">
+
+    <h1 class="text-center my-5">Usluge</h1>
+
+    <div class="service-header">
+        <h2>Šta je tecar energetska tehnologija?</h2>
+        <p class="py-2 fs-5">Jedna od najnovijih terapija u fizikalnon medicini i rehabilitaciji, Tecar energija je struja visoke frekvencije koja osciluje između 300 kHz i 1 MHz. Ubrzava „prirodnu regeneraciju“ organskog tkiva. Njegov nizak intenzitet znači da je energija potpuno neinvazivna i 100% prirodna za ljudsko telo.</p>
+
+        <p class="py-2 fs-5">Istraživanja sprovedena u poslednjih trideset godina pokazala su da njeni terapeutski efekti prirodno stimulišu regeneraciju tkiva i smanjuju bol.
+        </p>
+
+        <p class="py-2 fs-5">Brži funkcionalni oporavak. (Korisni efekti su očigledni nakon samo nekoliko sesija, a poboljšanja se mogu primetiti čak 24 sata nakon tretmana.)
+        </p>
+
+        <p class="fs-5">Tretman je bezbolan i opuštajući.
+        </p>
+
+        <p class="fs-5">Tretmani su sigurni i imaju visoku terapeutsku efikasnost.
+        </p>
+
+        
+        <p class="fs-5">Promoviše osećaj blagostanja, predisponirajući subjekta na fizičku aktivnost nakon tretmana.
+        </p>
+
+        <p class="fs-5">Brža rehabilitacija.
+        </p>
+        
+        <h4 class="py-3">Koje su prednosti za pacijente?</h4>
+        <p class="py-2 fs-5">Problemi sa mišićima, zglobovima i limfom:</p>
+
+        <p class="py-2 fs-5">Traumatologija</p>
+
+        <p class="py-2 fs-5">Reumatologija</p>
+
+        <p class="py-2 fs-5">Sportska fizioterapija
+        </p>
+
+        <p class="py-2 fs-5">Tecar je posebno koristan za ublažavanje bolova i protiv zapaljenja. Koristi se za lečenje akutnih ili hroničnih stanja u mišićima i zglobovima.
+        </p>
+
+        <p class="py-2 fs-5">Tehnologija pomaže da se prekine začarani krug bola, upale i ukočenosti. Pomaže kod drenaže izliva, poboljšava pokretljivost zglobova i podstiče proces zarastanja. Zauzvrat, ovo će smanjiti vreme oporavka i ubrzati povratak normalnim aktivnostima.</p>
+
+    </div>
+</div>
+
+</div>
+
+<?php include "kontakt.php"; 
+?>
+
+<?php
+include "footer.php"
+    ?>
