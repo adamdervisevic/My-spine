87015f987179d5119828ba1ad831dea84439a8d8
adamdervisevic <adamdervisevic@yahoo.com>
2024-12-24 10:58:39 +0700

    s

---
+++ tecar-terapija.php
@@ -0,0 +1,24 @@
+<?php
+include 'head.php';
+include 'navbar.php'; ?>
+<div class="container">
+    <h1 class="text-center my-5">Usluge</h1>
+    <h2 class="text-center">Šta je tecar energetska tehnologija?</h2>
+    
+    <p class="py-5 fs-5">Kineziterapija, takođe poznata kao kineziologija, je osnovni element fizikalne terapije (fizioterapije), koristeći aktivne i pasivne pokrete tela za jačanje mišića ili rehabilitaciju povreda i poboljšanje pokretljivosti, izdržljivosti i snage pojedinca. <br>
+    Sprovodi se individulano u zavisnosti od opšteg stanja pacijenta. <br> Prema nekim opisima, razlikuje se od fizikalne terapije po tome što fizikalna terapija stavlja akcenat na preventivnu negu, dok kineziterapija naglašava poboljšanje ukupne fizičke kondicije pacijenta.</p>
+
+    <h4 class="py-3">Od čega se sastoji kineziterapija?</h4>
+    <p class="py-2 fs-5">Kineziterapija se sastoji od tehnika rehabilitacije zasnovanih na pokretima tela za lečenje fizičkih oboljenja. <br> Ovo često uključuje istezanje mišića, tetiva i ligamenata, bilo aktivno (tj. pacijent sam izvodi istezanje) ili pasivno (fizioterapeut manipuliše svojim telom ručno). <br> Masaže se takođe mogu koristiti za manipulaciju mišićima pacijenta. <br> Pacijent će možda morati da izvodi vežbe dizajnirane da ojačaju mišiće, od kojih neke mogu uključivati upotrebu posebnih aparata. <br>
+    Za kineziterapiju nije potrebna posebna priprema, jer se radi o isključivo fizikalnoj terapiji koja ne koristi nikakve anestezije, aparate ili lekove.</p>
+
+    <h4 class="py-3">Stanja koja se leče kineziterapijom</h4>
+    <p class="py-2 fs-5">Koristi se za lečenje povreda i oboljenja različitog porekla, ali se pre svega preporučuje osobama sa mišićnim tegobama, kao što su tendonitis, plantarni fascitis, bol u leđima (lumbalni i cervikalni sindrom) i degenerativne bolesti mišićno-skeletnog sistema.</p>
+</div>
+
+<?php include "kontakt.php"; 
+?>
+
+<?php
+include "footer.php";
+?>
